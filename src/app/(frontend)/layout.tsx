88b3591cdffdb752cea05e84084b06d0b03bca98
Pablo Colaiacovo <2396949+pablocolaiacovo@users.noreply.github.com>
2025-04-16 00:21:52 +0200

    first commit

---
+++ src/app/(frontend)/layout.tsx
@@ -0,0 +1,10 @@
+import { SanityLive } from '@/sanity/lib/live'
+
+export default function FrontendLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      {children}
+      <SanityLive />
+    </>
+  )
+}
